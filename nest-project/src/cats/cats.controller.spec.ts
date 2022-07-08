32a867bc5656ed01cd5ef3e25cb8bfd9cf375995
HayataSato <hayatasato.0@gmail.com>
2022-07-08 23:55:54 +0900

    test: fix failed CatsController test to succeed

--- nest-project/src/cats/cats.controller.spec.ts
+++ nest-project/src/cats/cats.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsServiceBase } from './interfaces/cat-service.interface';
+import { CatsServiceMock } from './mocks/cats.service.mock';
 
 describe('CatsController', () => {
   let controller: CatsController;
@@ -7,6 +9,16 @@ describe('CatsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [
+        {
+          provide: CatsServiceBase,
+          useClass: CatsServiceMock,
+        },
+        {
+          provide: 'STRING_VAL_TOKEN',
+          useValue: '!!! Execute findAll handler in test !!!',
+        },
+      ],
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
